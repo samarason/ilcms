7cf6fff0caf029cd638251c2040ff19939842dc8
Sverrir Arason <sverrirarason@yahoo.com>
2026-09-24 17:17:40 +0200

    creating branch packages

--- .build-desktop-staging/msi/Uninstall-ILCMS.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Stopping any running ILCMS instances...
-taskkill /F /IM node.exe /FI "WINDOWTITLE eq ILCMS*" >nul 2>&1
-echo Done. Please use Windows Settings or Control Panel to complete uninstallation.
--- sql/00-init-keycloak.sql
+++
@@ -1,5 +0,0 @@
--- Initialize dedicated database for Keycloak OIDC
-SELECT 'CREATE DATABASE keycloak_db'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak_db')\gexec
-
-GRANT ALL PRIVILEGES ON DATABASE keycloak_db TO ilcms_user;
